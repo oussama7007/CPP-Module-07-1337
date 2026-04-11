5a9720a4061cbed476c5396a06bcf509e8aa19d7
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-11 18:49:35 +0100

    Editing ex00/a.out ex00/main.cpp,

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -33,17 +33,32 @@ std::ostream  & operator<<(std::ostream &out, const obj& b)
 
 template <typename S> void  swap(S& x, S& y)
 {
-    S tmp;
-    tmp = x ;
+    S tmp(x);
+    // tmp = x ;
     x = y ;
     y = tmp ;
     return ;
 }
 
+template <typename Y> Y min(Y x, Y y)
+{
+    if(x == y)
+        return y;
+    return (x < y ) ? x : y;
+} 
+
+template <typename M> M max(M x, M y)
+{
+    if(x == y)
+        return y;
+    return (x > y ) ? x : y;
+}
+
 int main()
 {
-    obj a;
-    obj b;
+    int  a = 100;
+    int  b = 9999;
+    std::cout << "a :"<< a << "B:" << b << std::endl; 
     swap(a, b);
     std::cout << "a :"<< a << "B:" << b << std::endl; 
 }
