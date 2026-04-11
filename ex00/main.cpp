ed8e85ced2fd11ba9ac3cd45662d6eb4569160e1
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-11 17:42:49 +0100

    Editing ex00/main.cpp,

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -9,19 +9,37 @@
 
 
 #include <iostream>
+
+class   obj
+{   
+    public:
+        int level;
+        obj() : level(789) {}
+        void printf()
+        {
+            std::cout << "the message is obj" << this << std::endl;
+        }
+};
+std::ostream  & operator<<(std::ostream &out, const obj& b)
+{
+    std::cout << "obj address :" << &b << std::endl;
+    return out ;
+}
+
 template <typename S> void  swap(S& x, S& y)
 {
-    S *tmp;
-    *tmp = *x ;
-    *x = *y ;
-    *y = *tmp ;
+    S tmp;
+    tmp = x ;
+    x = y ;
+    y = tmp ;
     return ;
 }
 
 int main()
 {
-    int a = 24;
-    int b = 22;
+    obj a;
+    obj b;
+    std::cout << "a :"<< a << "B:" << b << std::endl; 
     swap(a, b);
     std::cout << "a :"<< a << "B:" << b << std::endl; 
 }
