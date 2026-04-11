5843c4071894b050fe02cdd56dfeb96b8ce377de
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-11 18:16:13 +0100

    Editing ex00/main.cpp,

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -15,19 +15,14 @@ class   obj
     public:
         int level;
         obj() : level(789) {}
+        obj&    operator=(const obj& other)
+        {
+            this->level = other.level;
+            return *this;
+        }
         void printf()
         {
             std::cout << "the message is obj" << this << std::endl;
-        }
-};
-class j 
-{
-    public :
-        int level;
-        j() : level(9999) {}
-        void printf()
-        {
-            std::cout << "the message is j" << this << std::endl;
         }
 };
 std::ostream  & operator<<(std::ostream &out, const obj& b)
@@ -35,11 +30,7 @@ std::ostream  & operator<<(std::ostream &out, const obj& b)
     std::cout << "obj address :" << &b << std::endl;
     return out ;
 }
-std::ostream  & operator<<(std::ostream &out, const j& b)
-{
-    std::cout << "j address :" << &b << std::endl;
-    return out ;
-}
+
 template <typename S> void  swap(S& x, S& y)
 {
     S tmp;
@@ -52,8 +43,7 @@ template <typename S> void  swap(S& x, S& y)
 int main()
 {
     obj a;
-    j b;
-    std::cout << "a :"<< a << "B:" << b << std::endl; 
+    obj b;
     swap(a, b);
     std::cout << "a :"<< a << "B:" << b << std::endl; 
 }
