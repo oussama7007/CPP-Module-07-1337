462b7c3e4bfb40f640cc1a0c2de64fa377e1bc37
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-11 16:54:30 +0100

    Auto update 2026-04-11 16:54:30

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,3 +5,23 @@
 
 
 
+
+
+
+#include <iostream>
+template <typename S> void  swap(S& x, S& y)
+{
+    S *tmp;
+    *tmp = *x ;
+    *x = *y ;
+    *y = *tmp ;
+    return ;
+}
+
+int main()
+{
+    int a = 24;
+    int b = 22;
+    swap(a, b);
+    std::cout << "a :"<< a << "B:" << b << std::endl; 
+}
--- ex00/whatever.h
+++ ex00/whatever.h
@@ -4,3 +4,4 @@
 
 #pragma once 
 
+
