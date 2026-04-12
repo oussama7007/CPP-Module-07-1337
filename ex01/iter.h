0b96481fdfbe3a4ee0681327cf08d534b593fa3d
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-12 11:45:46 +0100

    Editing ex01/iter.h,

--- ex01/iter.h
+++ ex01/iter.h
@@ -11,7 +11,7 @@ template <typename I>
 
 I iter(I *arr,const std::size_t b, void (*f)(I&))
 {
-    for(int i = 0; i < b ; i++)
+    for(std::size_t i = 0; i < b ; i++)
     {
         f(arr[i]);
     }
