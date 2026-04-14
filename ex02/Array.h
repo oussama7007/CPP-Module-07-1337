66a417bbb8fb3db6c696ee8ecfe24231dca53696
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-14 07:59:11 +0100

    Editing ex02/Array.h,

--- ex02/Array.h
+++ ex02/Array.h
@@ -7,19 +7,20 @@
 
 # include <iostream>
 
-// TODO 1: Write the template header here (template <typename ...>)
+
 template <typename T>
 class Array {
     private:
-        // TODO 2: Declare a pointer to type T (this will hold your array)
-        // TODO 3: Declare an unsigned int to hold the size of the array
-        T* ptr;
-        unsigned int ezis;
+        T* ptrs;
+        unsigned int size;
     public:
         Array();
         Array(const Array& other);
         Array& operator=(const Array& other);
         ~Array();
+
+        Array(unsigned int n);
+        
 
 
 };
