f799ad4a6b5d735b72d98cb3f36f2a683cfffe76
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-14 07:42:28 +0100

    Editing ex02/Array.h,

--- ex02/Array.h
+++ ex02/Array.h
@@ -8,32 +8,22 @@
 # include <iostream>
 
 // TODO 1: Write the template header here (template <typename ...>)
+template <typename T>
 class Array {
     private:
         // TODO 2: Declare a pointer to type T (this will hold your array)
         // TODO 3: Declare an unsigned int to hold the size of the array
-
+        T* ptr;
+        unsigned int ezis;
     public:
-        // --- ORTHODOX CANONICAL FORM ---
-        
-        // TODO 4: Write the prototype for the Default Constructor
-        
-        // TODO 5: Write the prototype for the Copy Constructor 
-        // (Hint: It takes a const reference to another Array<T>)
-        
-        // TODO 6: Write the prototype for the Copy Assignment Operator
-        
-        // TODO 7: Write the prototype for the Destructor
+        Array();
+        Array(const Array& other);
+        Array& operator=(const Array& other);
+        ~Array();
 
-        // --- SUBJECT REQUIREMENTS ---
 
-        // TODO 8: Write the prototype for a constructor that takes an unsigned int 'n'
-        
-        // TODO 9: Write the prototype for the size() function. 
-        // (Hint: It returns an unsigned int and must be const)
 };
 
-// This tells the compiler: "Go look in the kitchen for the actual code!"
 
 
 #endif
