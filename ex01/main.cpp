f01e9b7b5f0309bf0834475217e21fd54f073407
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-14 22:45:04 +0100

    Editing ex01/main.cpp ex02/main.cpp,

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,41 +4,73 @@
 #include "iter.h"
 
 
-void divide_by2(int &i)
-{
-    i /= 2;
-}
+// void divide_by2(int &i)
+// {
+//     i /= 2;
+// }
 
-void camel(std::string &str)
-{
+// void camel(std::string &str)
+// {
     
-    for (std::size_t i = 0; i < str.length(); i++)
-    {
-        if (std::islower(str[i]))
-            str[i] = std::toupper(str[i]);
-        else if (std::isupper(str[i]))
-            str[i] = std::tolower(str[i]);  
-    }
-}
+//     for (std::size_t i = 0; i < str.length(); i++)
+//     {
+//         if (std::islower(str[i]))
+//             str[i] = std::toupper(str[i]);
+//         else if (std::isupper(str[i]))
+//             str[i] = std::tolower(str[i]);  
+//     }
+// }
 
-int main()
-{
-    std::cout << "================== Test string array ===================" << std::endl;
+// int main()
+// {
+//     std::cout << "================== Test string array ===================" << std::endl;
    
-    std::string str_arr[] = {"HelLow", "OusSamA", "What You FroM"};
+//     std::string str_arr[] = {"HelLow", "OusSamA", "What You FroM"};
     
-    ::iter(str_arr, 3, camel);
+//     iter(str_arr, 3, camel);
     
-    for (int i = 0; i < 3; i++)
-        std::cout << str_arr[i] << std::endl;
+//     for (int i = 0; i < 3; i++)
+//         std::cout << str_arr[i] << std::endl;
 
-     std::cout << "================== Test int  array ===================" << std::endl;
-    int int_arr[] = {10, 200, 300};
+//      std::cout << "================== Test int  array ===================" << std::endl;
+//     int int_arr[] = {10, 200, 300};
 
-    ::iter(int_arr, 3, divide_by2);
+//     iter(int_arr, 3, divide_by2);
     
-    for (int i = 0; i < 3; i++)
-        std::cout << int_arr[i] << std::endl;
+//     for (int i = 0; i < 3; i++)
+//         std::cout << int_arr[i] << std::endl;
 
-    return 0;
+//     return 0;
+// }
+
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
+}
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4 };
+  Awesome tab2[5];
+
+  iter( tab, 5, print<const int> );
+  iter( tab2, 5, print<Awesome> );
+
+  return 0;
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,42 +1,103 @@
 
+// #include "Array.h"
+
+
+
+// int main() 
+// {
+//     std::cout << "=== Test 1: Allocation & Size ===" << std::endl;
+//     Array<int> emptyArr;
+//     Array<int> numArr(5);
+    
+//     std::cout << "Size of emptyArr: " << emptyArr.size() << "(0)" << std::endl;
+//     std::cout << "Size of numArr: " << numArr.size() << " (5)" << std::endl;
+
+
+//     std::cout << "\n=== Test 2: Writing & Reading ===" << std::endl;
+//     for (unsigned int i = 0; i < numArr.size(); i++) {
+//         numArr[i] = i * 10;
+//         std::cout << "numArr[" << i << "] = " << numArr[i] << std::endl;
+//     }
+
+
+
+//     std::cout << "\n=== Test 3: Out of Bounds Exception ===" << std::endl;
+//     try {
+//         std::cout << "Attempting to write to numArr[10]..." << std::endl;
+//         numArr[10] = 999; 
+//         std::cout << "FAIL: The program did not stop you!" << std::endl;
+//     } 
+//     catch (const std::exception& e) {
+//         std::cout << "SUCCESS: Exception caught -> " << e.what() << std::endl;
+//     }
+
+
+
+
+//     std::cout << "\n=== Test 4: Deep Copy Verification ===" << std::endl;
+//     Array<int> copiedArr = numArr; 
+    
+//     std::cout << "Modifying copiedArr[0] to 42..." << std::endl;
+//     copiedArr[0] = 42;
+    
+//     std::cout << "Original numArr[0]: " << numArr[0] << " (Expected: 0)" << std::endl;
+//     std::cout << "Copied copiedArr[0]: " << copiedArr[0] << " (Expected: 42)" << std::endl;
+
+//     return 0;
+
+// }
+
+                                                                                         
+#include <iostream>
 #include "Array.h"
 
-
-
-int main() 
+#define MAX_VAL 750
+int main(int, char**)
 {
-    std::cout << "=== Test 1: Allocation & Size ===" << std::endl;
-    Array<int> emptyArr;
-    Array<int> numArr(5);
-    
-    std::cout << "Size of emptyArr: " << emptyArr.size() << " (Expected: 0)" << std::endl;
-    std::cout << "Size of numArr: " << numArr.size() << " (Expected: 5)" << std::endl;
-
-    std::cout << "\n=== Test 2: Writing & Reading ===" << std::endl;
-    // Fill the array with values
-    for (unsigned int i = 0; i < numArr.size(); i++) {
-        numArr[i] = i * 10;
-        std::cout << "numArr[" << i << "] = " << numArr[i] << std::endl;
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE                                                                                                                      
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
     }
 
-    std::cout << "\n=== Test 3: Out of Bounds Exception ===" << std::endl;
-    try {
-        std::cout << "Attempting to write to numArr[10]..." << std::endl;
-        numArr[10] = 999; 
-        std::cout << "FAIL: The program did not stop you!" << std::endl;
-    } 
-    catch (const std::exception& e) {
-        std::cout << "SUCCESS: Exception caught -> " << e.what() << std::endl;
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
     }
 
-    std::cout << "\n=== Test 4: Deep Copy Verification ===" << std::endl;
-    Array<int> copiedArr = numArr; // Calls the Copy Constructor
-    
-    std::cout << "Modifying copiedArr[0] to 42..." << std::endl;
-    copiedArr[0] = 42;
-    
-    std::cout << "Original numArr[0]: " << numArr[0] << " (Expected: 0)" << std::endl;
-    std::cout << "Copied copiedArr[0]: " << copiedArr[0] << " (Expected: 42)" << std::endl;
-
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//                                                                                                          
     return 0;
 }
