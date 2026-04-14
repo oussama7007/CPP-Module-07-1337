62c180dad3604d8e6595d6ca289cc9351f5c65ab
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-14 11:55:39 +0100

    Editing ex02/Array.h,

--- ex02/Array.h
+++ ex02/Array.h
@@ -6,7 +6,7 @@
 # define ARRAY_H
 
 # include <iostream>
-
+#include <stdexcept>
 
 template <typename T>
 class Array {
@@ -17,9 +17,9 @@ class Array {
     public:
         Array() : ptrs(NULL), size(0) {}
         
-        Array(unsigned int n);  data(new T[n]()), size(n) {}
+        Array(unsigned int n):  ptrs(new T[n]()), size(n) {}
         
-        Array(const Array& other) : data(new T[other.size]) , size(other.size) {
+        Array(const Array& other) : ptrs(new T[other.size]) , size(other.size) {
             for (std::size_t i = 0 ; i< size; i++)
             ptrs[i] = other.ptrs[i];
         }
@@ -46,12 +46,21 @@ class Array {
 
         T& operator[](unsigned int index) // bach tkheli user i dir modification inside array eg arr[0] = 42;
         {
-            if()
+            if(index >= size)
+                throw std::out_of_range("Index out of bounds");
+            return ptrs[index];
         }
         const T& operator[](unsigned int index) const; // bach the user i 9ra data mli tkon array as read only 
-
+        {
+            if(index >= size)
+                throw std::out_of_range("Index out of bounds");
+            return ptrs[index];
+        }
         // Size function (must be const!)
-        unsigned int size() const;
+        unsigned int size() const
+        {
+            return this->size ;
+        }
 
 };
 
