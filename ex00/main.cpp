ee62d5d59bd5869f4750ff00cadbe42433768e2f
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2026-04-15 15:15:40 +0100

    Editing ex00/main.cpp,, Deleting ex00/a,

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -16,15 +16,17 @@
 
 int main()
 {
-    const int a = 20;
-    const int b = 300;
-    swap( a, b );
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    int j = 20;
+    int l = 300;
+    swap( j, l );
+    std::cout << "j = " << j << ", l = " << l << std::endl;
+    const int a = 12;
+    const int b = 15;
+    std::cout << "min( a, b ) = " << min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << max( a, b ) << std::endl;
     std::string c = "chaine1";
     std::string d = "chaine2";
-    ::swap(c, d);
+    swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
